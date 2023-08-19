e4ef5edb41cf45cc3fad0f972fb954dbd8368fc3
Soham Chakraborty <81106480+Soham20chak@users.noreply.github.com>
2023-08-20 02:30:42 +0530

    Add files via upload

---
+++ haletech-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { CreateDoctorComponent } from './create-doctor/create-doctor.component';
+import { UpdateDoctorComponent } from './update-doctor/update-doctor.component';
+import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: 'doctors',component: DoctorsComponent},
+  //{path: '' , redirectTo: 'doctors', pathMatch:'full' } 
+  {path: 'create-doctors',component: CreateDoctorComponent},
+  {path: 'update-doctor/:id',component: UpdateDoctorComponent},
+  {path: 'doctor-details/:id',component: DoctorDetailsComponent},
+  {path: 'login',component:LoginComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ haletech-app/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { CreateDoctorComponent } from './create-doctor/create-doctor.component';
+import {FormsModule} from '@angular/forms';
+import { UpdateDoctorComponent } from './update-doctor/update-doctor.component';
+import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DoctorsComponent,
+    CreateDoctorComponent,
+    UpdateDoctorComponent,
+    DoctorDetailsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
